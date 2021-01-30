ac181417ae7fa4d4b18f8e7fda7be7dc0abb5d3b
Isaias Piña <iax7@users.noreply.github.com>
2021-01-29 20:33:10 -0600

    Add StoreSettings to Sanity

---
+++ sanity/schemas/storeSettings.js
@@ -0,0 +1,28 @@
+import { MdStore as icon } from 'react-icons/md';
+
+export default {
+  name: 'storeSettings', // Computer Name
+  title: 'Settings', // visible name
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Store Name',
+      type: 'string',
+      description: 'Name of the store',
+    },
+    {
+      name: 'slicemaster',
+      title: 'Slicemasters Currently Slicing',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'person' }] }],
+    },
+    {
+      name: 'hotSlices',
+      title: 'Hot Slices available in the case',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'pizza' }] }],
+    },
+  ],
+};
